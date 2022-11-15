6e247fcfec18e6887a3d2045349c6b49b46848a5
Ian Everitt <ian.andrew.everitt@gmail.com>
2022-11-15 17:43:10 -0600

    small addition of an http request to SWAPI. next, i need to implement something like pagination token functionality to get the response from each page of people in SWAPI

--- java_star_wars/src/main/java/JavaStarWars/java_star_wars/JSWController.java
+++ java_star_wars/src/main/java/JavaStarWars/java_star_wars/JSWController.java
@@ -2,6 +2,7 @@ package JavaStarWars.java_star_wars;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class JSWController {
@@ -16,5 +17,16 @@ public class JSWController {
         String[] jediStrings = {"Yoda", "Obi-Wan", "Anakin"};
 
         return jediStrings;
+    }
+
+    @GetMapping("/api/people")
+    public String people() {
+        // TODO: create a list of uris for each page of people OR look into pagination tokens or whatever
+        String uri = "https://www.swapi.tech/api/people";
+
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(uri, String.class);
+
+        return result;
     }
 }
