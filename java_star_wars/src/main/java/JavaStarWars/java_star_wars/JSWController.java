b3617cf746b5fb409530b4080d76b80e3ee9a5ef
Ian Everitt <ian.andrew.everitt@gmail.com>
2022-11-16 20:17:19 -0600

    added dependency for dealing with JSONObjects, added support for getting all pages of people from SWAPI and returning it to client as one response

--- java_star_wars/src/main/java/JavaStarWars/java_star_wars/JSWController.java
+++ java_star_wars/src/main/java/JavaStarWars/java_star_wars/JSWController.java
@@ -1,8 +1,17 @@
 package JavaStarWars.java_star_wars;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONArray;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import JavaStarWars.utils.entities.ResponseObject;
 
 @RestController
 public class JSWController {
@@ -20,13 +29,44 @@ public class JSWController {
     }
 
     @GetMapping("/api/people")
-    public String people() {
-        // TODO: create a list of uris for each page of people OR look into pagination tokens or whatever
-        String uri = "https://www.swapi.tech/api/people";
+    public ResponseObject  people() {
+        try {
+            // array of all endpoints needed to hit
+            String[] uris = { 
+                "https://www.swapi.tech/api/people", 
+                "https://www.swapi.tech/api/people?page=2&limit=10", 
+                "https://www.swapi.tech/api/people?page=3&limit=10", 
+                "https://www.swapi.tech/api/people?page=4&limit=10",
+                "https://www.swapi.tech/api/people?page=5&limit=10",
+                "https://www.swapi.tech/api/people?page=6&limit=10",
+                "https://www.swapi.tech/api/people?page=7&limit=10",
+                "https://www.swapi.tech/api/people?page=8&limit=10",
+                "https://www.swapi.tech/api/people?page=9&limit=10"
+            };
+    
+            RestTemplate restTemplate = new RestTemplate();
+            
+            // ResponseObject is a custom class found in ./utils/entities
+            ResponseObject results = new ResponseObject();
+            
+            for (String uri : uris) {
+                // get one page list of people from SWAPI 
+                String response = restTemplate.getForObject(uri, String.class);
+                // convert resulting String into JSON
+                JSONObject responseJSON = new JSONObject(response);
+                // extract just the array of people
+                JSONArray peopleArray = responseJSON.getJSONArray("results");
+    
+                // add new array of people to results to be returned
+                results.responseList.add(peopleArray.toString());
+            }
+    
+            return results;
+        } catch (JSONException e) {
+            ResponseObject errorResponse = new ResponseObject();
+            errorResponse.jsonException = e;
 
-        RestTemplate restTemplate = new RestTemplate();
-        String result = restTemplate.getForObject(uri, String.class);
-
-        return result;
+            return errorResponse;
+        }
     }
 }
---
+++ java_star_wars/src/main/java/JavaStarWars/utils/entities/ResponseObject.java
@@ -0,0 +1,9 @@
+package JavaStarWars.utils.entities;
+import java.util.ArrayList;
+
+import org.json.JSONException;
+
+public class ResponseObject {
+    public ArrayList<String> responseList = new ArrayList<>();
+    public JSONException jsonException;
+}
